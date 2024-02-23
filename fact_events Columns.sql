05641c7524fbd92b2ab3a19df52f55778263be70
PrashanthPPK <149346388+PrashanthPPK@users.noreply.github.com>
2024-02-22 19:20:06 -0500

    fact_events Columns.sql

--- fact_events Columns.sql
+++ fact_events Columns.sql
@@ -7,8 +7,8 @@ UPDATE fact_events SET Revenue_BeforePromo = base_price * `quantity_sold(before_
 ALTER TABLE fact_events ADD COLUMN discount_price int;
 
 UPDATE fact_events SET discount_price = base_price * 0.5 WHERE promo_type = '50% OFF';
-UPDATE fact_events SET discount_price = base_price * 0.25 WHERE promo_type = '25% OFF';
-UPDATE fact_events SET discount_price = base_price * 0.33 WHERE promo_type = '33% OFF';
+UPDATE fact_events SET discount_price = base_price * 0.75 WHERE promo_type = '25% OFF';
+UPDATE fact_events SET discount_price = base_price * 0.67 WHERE promo_type = '33% OFF';
 UPDATE fact_events SET discount_price = base_price - 500 WHERE promo_type = '500 Cashback';
 UPDATE fact_events SET discount_price = base_price * 0.5 WHERE promo_type = 'BOGOF';
 
@@ -21,3 +21,6 @@ ALTER TABLE fact_events ADD COLUMN IR int;
 ALTER TABLE fact_events ADD COLUMN ISU int;
 UPDATE fact_events SET IR =Revenue_AfterPromo - Revenue_BeforePromo ; 
 UPDATE fact_events SET ISU = `quantity_sold(after_promo)` - `quantity_sold(before_promo)`; 
+
+
+UPDATE fact_events SET `quantity_sold(after_promo)` = `quantity_sold(after_promo)` * 2 WHERE promo_type = 'BOGOF';
